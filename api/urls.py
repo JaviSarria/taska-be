4eb2e093ff1ea11e9efdd99c28c93c9f90da2d59
Jose Javier Sarria Garcia <es27343943w@enel.com>
2024-03-11 18:01:10 +0100

    viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers
+from .views import TaskViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls)),
 ]
